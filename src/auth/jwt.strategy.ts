626ed38130d2af1e727d109a2b6563f685074641
NuZard84 <patelhetvarasada@gmail.com>
2025-09-17 11:35:40 +0530

    Add : add phone num in valid function of JWT strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,6 +14,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
+    return {
+      userId: payload.sub ?? null,
+      email: payload.email ?? null,
+      phoneNumber: payload.phoneNumber ?? null,
+    };
   }
 }
